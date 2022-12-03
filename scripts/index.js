64f4e4d212b5b11de7bf1968adb0dd46aff37bc7
i-t <s_3i@mail.ru>
2022-12-03 09:14:47 +0300

    hotfix. заменен шрифт, <button>, обновлен js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 const profile = document.querySelector('.profile');
 const popup = document.querySelector('.popup');
-const post = document.querySelector('.post')
 
 let profileName = profile.querySelector('.profile__name');
 let profileSubtext = profile.querySelector('.profile__subtext');
@@ -12,13 +11,15 @@ let popupCloseButton = popup.querySelector('.popup__close');
 let pageName = document.querySelector('title');
 console.log(pageName.textContent);
 
-let nameInput = popup.querySelector('.popup__input_type-name');
-let subtextInput = popup.querySelector('.popup__input_type-subtext');
+let nameInput = popup.querySelector('.popup__input_edit-name');
+let subtextInput = popup.querySelector('.popup__input_edit-subtext');
 
 let likeButtons = document.querySelectorAll('.post__like-btn');
 
 
 function popupOpen() {
+  nameInput.value = profileName.textContent;
+  subtextInput.value = profileSubtext.textContent;
   popup.classList.add('popup_opened');
 }
 
@@ -32,12 +33,6 @@ function handleFormSubmit(evt) {
   profileName.textContent = nameInput.value;
   profileSubtext.textContent = subtextInput.value;
   popupClose();
-}
-
-for (let i = 0; i < likeButtons.length; i++) {
-  likeButtons[i].addEventListener('click', function likePost() {
-    likeButtons[i].classList.toggle('post__like-btn_pushed');
-  });
 }
 
 
