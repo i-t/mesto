949a4d5338f8379c1187a4a28798790a54f71385
i-t <s_3i@mail.ru>
2022-12-21 07:58:10 +0300

    Посты JS. Удаление. Лайки

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,82 @@
-const profile = document.querySelector('.profile');
+const profileName = document.querySelector('.profile__name');
+const profileSubtext = document.querySelector('.profile__subtext');
+
+const addButton = document.querySelector('.profile__add-btn');
+const editButton = document.querySelector('.profile__edit-btn');
+
+
+const initialPosts = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+// Раздел с постами
+const postsSection = document.querySelector('.posts'); 
+const template = document.querySelector('#post-template');
+
+// Создание постов с массива
+const createPost = (name, link) => {
+  const post = template.content.querySelector('.post').cloneNode(true);
+
+  post.querySelector('.post__title').textContent = name;
+  post.querySelector('.post__photo').alt = `${name}`;
+  post.querySelector('.post__photo').src = link;
+
+  post.querySelector('.post__del-btn').addEventListener('click', () => {post.remove();});
+
+  post.querySelector('.post__like-btn').addEventListener('click', () => {
+    post.querySelector('.post__like-btn').classList.toggle('post__like-btn_pushed');
+  });
+  // Лайк с объявлением переменной
+  // const likeButton = post.querySelector('.post__like-btn');
+  // likeButton.addEventListener('click', () => {
+  //   likeButton.classList.toggle('post__like-btn_pushed')
+  // });
+  
+
+  return post;
+}
+
+// Добавление поста
+const renderPost = (name, link) => { 
+  postsSection.append(createPost(name, link));
+}
+
+initialPosts.forEach(({name, link}) => {
+  renderPost(name, link);
+})
+
+
+
+
+// Попап
+const popupAdd = document.querySelector('.popup_add-post');
+const popupEdit = document.querySelector('.popup_edit-profile');
 const popup = document.querySelector('.popup');
-
-let profileName = profile.querySelector('.profile__name');
-let profileSubtext = profile.querySelector('.profile__subtext');
-let profileEditButton = profile.querySelector('.profile__edit-btn');
-
-let popupSaveButton = popup.querySelector('.popup__save');
-let popupCloseButton = popup.querySelector('.popup__close');
+const popupSaveButton = popup.querySelector('.popup__save');
+const closeButton = popup.querySelector('.popup__close');
 
 let pageName = document.querySelector('title');
 console.log(pageName.textContent);
@@ -14,12 +84,13 @@ console.log(pageName.textContent);
 let nameInput = popup.querySelector('.popup__input_edit_name');
 let subtextInput = popup.querySelector('.popup__input_edit_subtext');
 
-let likeButtons = document.querySelectorAll('.post__like-btn');
 
+
+function likePost() {
+  popup.classList.add('post__like-btn_pushed');
+}
 
 function popupOpen() {
-  nameInput.value = profileName.textContent;
-  subtextInput.value = profileSubtext.textContent;
   popup.classList.add('popup_opened');
 }
 
@@ -36,8 +107,17 @@ function handleFormSubmit(evt) {
 }
 
 
-profileEditButton.addEventListener('click', popupOpen);
 
-popupCloseButton.addEventListener('click', popupClose);
+
+editButton.addEventListener('click', () => {
+  nameInput.value = profileName.textContent;
+  subtextInput.value = profileSubtext.textContent;
+  popupOpen();
+});
+
+
+addButton.addEventListener('click', popupOpen);
+
+closeButton.addEventListener('click', popupClose);
 
 popup.addEventListener('submit', handleFormSubmit);
