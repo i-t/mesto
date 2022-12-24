687bd9995f7ff96bcc0e755018812bc3f927fd9b
i-t <s_3i@mail.ru>
2022-12-24 17:58:54 +0300

    Устранение замечаний после кодревью

---
+++ scripts/initial-posts.js
@@ -0,0 +1,26 @@
+const initialPosts = [
+  {
+    name: 'Эвенкия',
+    link: './images/01_krasavia_evenkya.jpg'
+  },
+  {
+    name: 'Аэропорт Горный',
+    link: './images/09_krasavia-tura-gornyi.jpg'
+  },
+  {
+    name: 'Тайга',
+    link: './images/07_krasavia_taiga.jpg'
+  },
+  {
+    name: 'Тутончаны',
+    link: './images/55_krasavia-tutonchani.jpg'
+  },
+  {
+    name: 'Река Иритка',
+    link: './images/64_krasavia_iritka_river.jpg'
+  },
+  {
+    name: 'КрасАвиа',
+    link: './images/00_krasavia.jpg'
+  }
+]; 
